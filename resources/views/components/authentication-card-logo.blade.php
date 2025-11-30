8d713937c1fe80a618f04e403aaecbb3354673c3
George Dobre <george.dobre@me.com>
2025-11-30 04:37:26 +0000

    Updated card logo

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,7 +1,3 @@
 <a href="/" class="inline-flex items-center gap-4 text-white">
     <img src="/logo/dynamik-logo-transparent.png" alt="DYNAMIK" class="h-12 w-auto drop-shadow-md">
-    <div class="text-left">
-        <p class="text-xl font-semibold leading-tight">DYNAMIK</p>
-        <p class="text-sm text-gray-300">The Power to Connect</p>
-    </div>
 </a>
